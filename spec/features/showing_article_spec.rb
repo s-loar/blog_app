3286f576b3913bd35acd3c1fd24a52bc612ebfc8
Steve Loar <sjl7909@gmail.com>
2015-12-22 19:37:59 -0500

    Restrict new and edit and delete options

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -2,7 +2,30 @@ require 'rails_helper'
 
 RSpec.feature "Showing an Article" do
   before do
-    @article = Article.create(title: "The First Article", body: "Body of first article")
+    @john = User.create!(email: "john@example.com", password: "password")
+    @fred = User.create!(email: "fred@example.com", password: "password")
+    @article = Article.create(title: "The First Article", body: "Body of first article", user: @john)
+  end
+
+  scenario "A non-signed in user does not see edit and delete links" do
+    visit "/"
+    click_link @article.title
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "A non-owner signed in does not see edit and delete links" do
+    login_as(@fred)
+    visit "/"
+    click_link @article.title
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
   end
 
   scenario "Display individual article" do
