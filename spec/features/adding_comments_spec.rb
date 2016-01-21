5004c2b55cee4793e853c045b2449a122bec78bc
Steve Loar <sjl7909@gmail.com>
2016-01-21 16:37:26 -0500

    Code to add comment on the view and spec

---
+++ spec/features/adding_comments_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Adding Comments to Articles" do
+
+  before do
+    @john = User.create!(email: "john@example.com", password: "password")
+    @fred = User.create!(email: "fred@example.com", password: "password")
+    @article = Article.create(title: 'First Article', body: 'Body of first article', user: @john)
+    # login_as(@john)
+  end
+
+  scenario "permits a signed in user to write a review" do
+    login_as(@fred)
+    visit "/"
+    click_link @article.title
+    fill_in "New Comment", with: "An Awsome Article!"
+    click_button "Add Comment"
+
+    expect(page).to have_content("Comment has been created")
+    expect(page).to have_content("An Awsome Article!")
+    expect(page.current_path).to eq(articles_path(@article.comments.last.id))
+  end
+
+end
