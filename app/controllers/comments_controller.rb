064fdfe7dd189150258c1000fa4a9792a44ebeee
Steve Loar <sjl7909@gmail.com>
2016-01-25 14:34:01 -0500

    Handle not signed in user posting a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,21 @@ class CommentsController < ApplicationController
   before_action :set_article
 
   def create
-    @comment = @article.comments.build(comment_params)
-    @comment.user = current_user
-
-    if @comment.save
-      flash[:success] = "Comment has been created"
+    unless current_user
+      flash[:danger] = "Please sign in or sign up first"
+      redirect_to new_user_session_path
     else
-      flash.now[:danger] = "Comment has not been created"
-    end
+      @comment = @article.comments.build(comment_params)
+      @comment.user = current_user
 
-    redirect_to article_path(@article)
+      if @comment.save
+        flash[:success] = "Comment has been created"
+      else
+        flash.now[:danger] = "Comment has not been created"
+      end
+
+      redirect_to article_path(@article)
+    end
   end
 
   private
